48215a891ac89da2b8987a6bba9b38266b8d6746
ecem.konu <konu.ecem@gmail.com>
2023-07-10 21:19:59 +0200

    starter code for user input

--- README.md
+++
@@ -1,1 +0,0 @@
-# dps_challenge
---
+++ index.js
@@ -0,0 +1,4 @@
+function getResults() {
+    var userInput = document.getElementById("userInput").value;
+    document.getElementById("output").innerHTML = "You entered: " + userInput;
+  }
